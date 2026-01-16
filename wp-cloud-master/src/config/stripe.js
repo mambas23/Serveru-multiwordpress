825ab48431d51317cc77c0de33839fc29af63a10
mambas23 <72620285+mambas23@users.noreply.github.com>
2026-01-16 03:17:33 +0100

    Delete wp-cloud-master directory

--- wp-cloud-master/src/config/stripe.js
+++
@@ -1,5 +0,0 @@
-export const STRIPE_PAYMENT_LINKS = {
-  "storage-5gb": "https://buy.stripe.com/test_8x214g4SXh0932L9i1eAg02",
-  "storage-2gb": "https://buy.stripe.com/test_bJe9AM859cJT7j1bq9eAg01",
-  "storage-1gb": "https://buy.stripe.com/test_00wfZa7154dngTBeCleAg00",
-};
--- wp-cloud-master/src/lib/stripe.js
+++
@@ -1,5 +0,0 @@
-import { loadStripe } from "@stripe/stripe-js";
-
-export const stripePromise = loadStripe(
-  "pk_test_51RZJrS4ZFdtcfFjRthv4znTOREhGT46RpfNAhoWvwUwLzWPWcvSvgrHRGtszMOEnXKjORXhfY3McyH0zJxZmrX1h00l3p3hZVG"
-);
